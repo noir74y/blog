2af15a9efbed5ad9e51889ab9ab737a5ea169f8b
noir74y <noir74@yandex.ru>
2025-01-19 15:43:33 +0300

    ref: changes in configuration classes

--- src/main/java/ru/noir74/blog/configuration/WebApplicationConfig.java
+++ src/main/java/ru/noir74/blog/configuration/WebApplicationConfig.java
@@ -1,4 +1,4 @@
-package ru.noir74.blog;
+package ru.noir74.blog.configuration;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = "ru.noir74.blog")
 @PropertySource("classpath:application.properties")
-public class ApplicationConfig {
+public class WebApplicationConfig {
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd hh:mm:ss";
 
     @Bean
