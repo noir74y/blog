dba259d7b963236e1172fee8757afcfe6945c8ec
noir74y <noir74@yandex.ru>
2025-02-15 23:02:17 +0300

    feat: запуск через web.xml (finally)

--- src/main/java/ru/noir74/blog/Main.java
+++ src/main/java/ru/noir74/blog/Main.java
@@ -8,9 +8,7 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) throws LifecycleException {
         Tomcat tomcat = new Tomcat();
-        tomcat.setSilent(true);
         tomcat.getConnector().setPort(9090);
-        tomcat.getConnector().setURIEncoding("UTF-8");
         tomcat.addWebapp("", new File("src/main/webapp/").getAbsolutePath());
         tomcat.start();
         tomcat.getServer().await();
