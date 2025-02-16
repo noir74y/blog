19247da19229f104d37ce8a6772edb60deaebd7a
noir74y <noir74@yandex.ru>
2025-02-16 12:12:02 +0300

    feat: front seems to be ready (plus support Ctrl-Enter)

--- src/main/java/ru/noir74/blog/Main.java
+++ src/main/java/ru/noir74/blog/Main.java
@@ -8,6 +8,7 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) throws LifecycleException {
         Tomcat tomcat = new Tomcat();
+        tomcat.setSilent(true);
         tomcat.getConnector().setPort(9090);
         tomcat.addWebapp("", new File("src/main/webapp/").getAbsolutePath());
         tomcat.start();
