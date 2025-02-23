79b42451df829e1760f35bd66d7d2c0bdf57bf26
noir74y <noir74@yandex.ru>
2025-02-24 00:00:04 +0300

    ref: more preparations for mvc configs

---
+++ src/test/java/ru/noir74/blog/test/configurations/MvcTestConfig.java
@@ -0,0 +1,10 @@
+package ru.noir74.blog.test.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class MvcTestConfig {
+}
--- src/test/java/ru/noir74/blog/test/tests/mvc/GenericMvcTest.java
+++ src/test/java/ru/noir74/blog/test/tests/mvc/GenericMvcTest.java
@@ -1,13 +1,22 @@
 package ru.noir74.blog.test.tests.mvc;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.context.web.WebAppConfiguration;
+import ru.noir74.blog.test.configurations.MvcTestConfig;
 import ru.noir74.blog.test.configurations.old.DaoLayerConfig;
 
-@SpringJUnitConfig(classes = DaoLayerConfig.class)
-@ExtendWith(MockitoExtension.class)
 @WebAppConfiguration
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {MvcTestConfig.class})
+@TestPropertySource(locations = "classpath:test-application.properties")
 public abstract class GenericMvcTest {
+    @Test
+    void test() {
+    }
 }
