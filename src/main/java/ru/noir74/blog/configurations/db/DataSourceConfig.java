dda7570d62ed0be01c4b6728450ad8aa7ee0743d
noir74y <noir74@yandex.ru>
2025-02-27 20:00:40 +0300

    feat: to add init sql script for prod

--- src/main/java/ru/noir74/blog/configurations/db/DataSourceConfig.java
+++ src/main/java/ru/noir74/blog/configurations/db/DataSourceConfig.java
@@ -3,7 +3,9 @@ package ru.noir74.blog.configurations.db;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import javax.sql.DataSource;
 
@@ -15,13 +17,19 @@ public class DataSourceConfig {
             @Value("${spring.datasource.url}") String dbUrl,
             @Value("${spring.datasource.username}") String dbUser,
             @Value("${spring.datasource.password}") String dbPass,
-            @Value("${spring.datasource.driver-class-name}") String dbDriverClassName
+            @Value("${spring.datasource.driver-class-name}") String dbDriverClassName,
+            @Value("${spring.datasource.init-sql-script}") String initSqlScript
     ) {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(dbDriverClassName);
         dataSource.setUrl(dbUrl);
         dataSource.setUsername(dbUser);
         dataSource.setPassword(dbPass);
+
+        ResourceDatabasePopulator loader = new ResourceDatabasePopulator();
+        loader.addScript(new ClassPathResource(initSqlScript));
+        loader.execute(dataSource);
+
         return dataSource;
     }
 }
--- src/test/java/ru/noir74/blog/test/configurations/DbContentConfig.java
+++ src/test/java/ru/noir74/blog/test/configurations/DbContentConfig.java
@@ -1,6 +1,7 @@
 package ru.noir74.blog.test.configurations;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
@@ -10,6 +11,7 @@ import javax.sql.DataSource;
 
 @Configuration
 @Import(DataSourceConfig.class)
+@DependsOn("DataSourceConfig")
 public class DbContentConfig {
     public ResourceDatabasePopulator resourceDatabasePopulator(DataSource dataSource) {
         ResourceDatabasePopulator loader = new ResourceDatabasePopulator();
