6efbc270d17373e8030089c0a5b57ff87074e6d8
noir74y <noir74@yandex.ru>
2025-02-27 16:27:54 +0300

    feat: small changes in MvcTestConfig

--- src/test/java/ru/noir74/blog/test/configurations/MvcTestConfig.java
+++ src/test/java/ru/noir74/blog/test/configurations/MvcTestConfig.java
@@ -1,12 +1,15 @@
 package ru.noir74.blog.test.configurations;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import ru.noir74.blog.configurations.WebConfig;
+import ru.noir74.blog.configurations.root.ModelMapperConfig;
 
 @Configuration
 @EnableWebMvc
-@Import({WebConfig.class, DaoTestConfig.class})
+@Import({WebConfig.class, DaoTestConfig.class, ModelMapperConfig.class})
+@ComponentScan("ru.noir74.blog.mappers")
 public class MvcTestConfig {
 }
