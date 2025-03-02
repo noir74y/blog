ae8fa22554b4ae5a00cf45be5e47544eb585939c
noir74y <noir74@yandex.ru>
2025-03-02 14:17:32 +0300

    initial commit

---
+++ src/test/java/ru/noir74/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.noir74.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
