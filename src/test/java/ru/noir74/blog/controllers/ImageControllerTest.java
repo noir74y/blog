3648513bea7e06d2895829cbd9a707953c5c82ff
noir74y <noir74@yandex.ru>
2025-03-06 19:19:35 +0300

    feat: controllers test are ready (small ref)

--- src/test/java/ru/noir74/blog/controllers/ImageControllerTest.java
+++ src/test/java/ru/noir74/blog/controllers/ImageControllerTest.java
@@ -3,55 +3,58 @@ package ru.noir74.blog.controllers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.noir74.blog.models.post.PostImage;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = ImageController.class)
 public class ImageControllerTest extends GenericControllerTest {
     Integer postId;
+    MockMultipartFile mockMultipartFile;
 
     @BeforeEach
     void setUp() {
         postId = 1;
-    }
-
-    @Test
-    void getImage() throws Exception {
-
-//        var mockMultipartFile = new MockMultipartFile(
-//                "file",
-//                "someFile.jpeg",
-//                "image/jpeg",
-//                new byte[]{(byte) 0x00});
-//
-//        when(postService.findImageById(PostImage.builder().id(postId).response(response).build())).thenReturn(Post.builder().build());
-
-        MockHttpServletResponse mockHttpServletResponse = mockMvc.perform(MockMvcRequestBuilders.get("/" + postId + "/image"))
-                .andExpect(status().isOk())
-                .andReturn().getResponse();
-
-        //verify(postService).findImageById(PostImage.builder().id(postId).response(mockHttpServletResponse).build());
-    }
-
-    @Test
-    void setImage() throws Exception {
-        var mockMultipartFileToBeSaved = new MockMultipartFile(
+        mockMultipartFile = new MockMultipartFile(
                 "file",
                 "someFile.jpeg",
                 "image/jpeg",
                 new byte[]{(byte) 0x00});
+    }
 
+    @Test
+    void getImage() throws Exception {
+        when(postService.findImageById(postId)).thenReturn(PostImage.builder()
+                .id(postId)
+                .image(mockMultipartFile.getBytes())
+                .imageName(mockMultipartFile.getOriginalFilename())
+                .build());
+
+        byte[] byteReceived = mockMvc.perform(MockMvcRequestBuilders.get("/" + postId + "/image"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsByteArray();
+
+        verify(postService).findImageById(postId);
+        assertArrayEquals(mockMultipartFile.getBytes(), byteReceived);
+    }
+
+    @Test
+    void setImage() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.multipart("/" + postId + "/image")
-                        .file(mockMultipartFileToBeSaved)
+                        .file(mockMultipartFile)
                         .param("id", String.valueOf(postId))
                 )
                 .andExpect(status().isOk());
 
-        verify(postService).setImageById(PostImage.builder().id(postId).file(mockMultipartFileToBeSaved).build());
+        verify(postService).setImageById(PostImage.builder()
+                .id(postId)
+                .image(mockMultipartFile.getBytes())
+                .imageName(mockMultipartFile.getOriginalFilename())
+                .build());
     }
 }
