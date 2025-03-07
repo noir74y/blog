79483d60923738eee7b0945931967916fe602f72
noir74y <noir74@yandex.ru>
2025-03-08 01:01:21 +0300

    ref: to optimize context configuration foe service tests

--- src/test/java/ru/noir74/blog/generics/ServiceTest.java
+++ src/test/java/ru/noir74/blog/generics/ServiceTest.java
@@ -3,6 +3,8 @@ package ru.noir74.blog.generics;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import ru.noir74.blog.configurations.ModelMapperConfig;
 import ru.noir74.blog.mappers.CommentMapper;
 import ru.noir74.blog.mappers.PostImageMapper;
 import ru.noir74.blog.mappers.PostMapper;
@@ -10,11 +12,20 @@ import ru.noir74.blog.mappers.TagMapper;
 import ru.noir74.blog.repositories.intf.CommentRepository;
 import ru.noir74.blog.repositories.intf.PostRepository;
 import ru.noir74.blog.repositories.intf.TagRepository;
+import ru.noir74.blog.services.impl.CommentServiceImpl;
+import ru.noir74.blog.services.impl.PostServiceImpl;
+import ru.noir74.blog.services.impl.TagServiceImpl;
 import ru.noir74.blog.services.intf.CommentService;
 import ru.noir74.blog.services.intf.PostService;
 import ru.noir74.blog.services.intf.TagService;
 
 @SpringBootTest
+@ContextConfiguration(classes = {
+        ModelMapperConfig.class,
+        TagServiceImpl.class,
+        CommentServiceImpl.class,
+        PostServiceImpl.class
+})
 public abstract class ServiceTest {
     @Autowired
     protected CommentMapper commentMapper;
