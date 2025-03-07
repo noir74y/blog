405bd881f9849825e9ed2add9379acd4d35b7d3e
noir74y <noir74@yandex.ru>
2025-03-07 22:16:28 +0300

    ref: to optimize dao tests

--- src/test/java/ru/noir74/blog/generics/DaoTest.java
+++ src/test/java/ru/noir74/blog/generics/DaoTest.java
@@ -3,13 +3,15 @@ package ru.noir74.blog.generics;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
 import ru.noir74.blog.repositories.intf.PostRepository;
 
 @DataJpaTest
-@ComponentScan(basePackages = {"ru.noir74.blog.repositories"})
-//@ContextHierarchy(@ContextConfiguration(name = "dao", classes = DaoTestConfig.class))
+@ContextHierarchy(@ContextConfiguration(name = "dao", classes = DaoTestConfig.class))
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class DaoTest extends Test {
     @Autowired
@@ -18,4 +20,7 @@ public abstract class DaoTest extends Test {
     protected PostRepository postRepository;
 }
 
-
+@TestConfiguration
+@ComponentScan(basePackages = {"ru.noir74.blog.repositories"})
+class DaoTestConfig {
+}
--- src/test/java/ru/noir74/blog/generics/TestConfig.java
+++
@@ -1,7 +0,0 @@
-package ru.noir74.blog.generics;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration
-public abstract class TestConfig {
-}
